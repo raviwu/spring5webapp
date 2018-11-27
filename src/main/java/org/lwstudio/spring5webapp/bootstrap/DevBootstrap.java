40ace35293e7b89b6c2c5541528e672ba68aecb9
Ravi Wu <raviwu@gmail.com>
2018-11-27 11:23:02 +0800

    Integrate Publisher to Repository and DevBootstrap
    - Implement Repository for Publisher
    - Update Book model to use Publisher relation `@OneToOne`
    - Revise DevBootstrap for Publisher

--- src/main/java/org/lwstudio/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/org/lwstudio/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package org.lwstudio.spring5webapp.bootstrap;
 
 import org.lwstudio.spring5webapp.model.Author;
 import org.lwstudio.spring5webapp.model.Book;
+import org.lwstudio.spring5webapp.model.Publisher;
 import org.lwstudio.spring5webapp.repositories.AuthorRepository;
 import org.lwstudio.spring5webapp.repositories.BookRepository;
+import org.lwstudio.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,19 +30,23 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
         // Eric
+        Publisher harper = new Publisher("Harper Collins", "US");
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "12345", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "12345", harper);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        publisherRepository.save(harper);
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
         // Rod
+        Publisher worx = new Publisher("Worx", "EU");
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", worx);
         rod.getBooks().add(noEJB);
 
+        publisherRepository.save(worx);
         authorRepository.save(rod);
         bookRepository.save(noEJB);
     }
--- src/main/java/org/lwstudio/spring5webapp/model/Publisher.java
+++ src/main/java/org/lwstudio/spring5webapp/model/Publisher.java
@@ -15,6 +15,11 @@ public class Publisher {
     private String name;
     private String address;
 
+    public Publisher(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
     public Long getId() {
         return id;
     }
